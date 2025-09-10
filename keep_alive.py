732367fa9289d71df330e1be48c7a0b92e089ea6
hgzy0987 <topup.buzz.bd1@gmail.com>
2025-09-10 19:25:42 +0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+# keep_alive.py
 from flask import Flask
 from threading import Thread
 
@@ -5,10 +6,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "✅ Bot is alive!"
+    return "SSS TikTok Download Bot is running!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
